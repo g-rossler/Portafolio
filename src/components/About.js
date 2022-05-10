4239070794512175dfe580b14fd88168cbe438fd
Gerardo Rossler <rossler.gerardo.dev@gmail.com>
2022-05-10 18:37:52 -0300

    Divide the Main component into 3 components

---
+++ src/components/About.js
@@ -0,0 +1,25 @@
+function About() { /*
+    <section id="main-about-me">
+      <h2 id="main-about-me-title">ABOUT ME</h2>
+      <p id="main-about-me-subtitle">I am a software developer</p>
+      <div id="main-technologies">
+        <h2 id="main-technologies-title">TECHNOLOGIES</h2>
+        <div id="main-technologies-list">
+          <div className="main-technologies-list-item">HTML</div>
+          <div className="main-technologies-list-item">JavaScript</div>
+          <div className="main-technologies-list-item">CSS</div>
+          <div className="main-technologies-list-item">React</div>
+          <div className="main-technologies-list-item">Node.JS</div>
+          <div className="main-technologies-list-item">Cypress</div>
+          <div className="main-technologies-list-item">NPM</div>
+          <div className="main-technologies-list-item">Babel</div>
+          <div className="main-technologies-list-item">jQuery</div>
+          <div className="main-technologies-list-item">Git</div>
+          <div className="main-technologies-list-item">GitHub</div>
+          <div className="main-technologies-list-item">Jest</div>
+          <div className="main-technologies-list-item">BootStrap</div>
+        </div>
+      </div>
+</section> */ }
+
+export default About;
---
+++ src/components/Proyects.js
@@ -0,0 +1,64 @@
+function Proyects() { /*
+
+    <section id="main-proyect">
+      <h2 id="main-proyect-title">PROYECTS</h2>
+      <div id="main-proyect-list">
+        <div className="main-proyect-list-item">
+          <h4 className="main-proyect-list-item-title">Pokedex</h4>
+          <p className="main-proyect-list-item-subtitle">example text</p>
+          <div className="main-proyect-list-item-group-button">
+            <button type="button" className="main-proyect-list-item-button">DEMO</button>
+            <button type="button" className="main-proyect-list-item-button">GITHUB</button>
+          </div>
+        </div>
+        <div className="main-proyect-list-item">
+          <h4 className="main-proyect-list-item-title">Portafolio</h4>
+          <p className="main-proyect-list-item-subtitle">example text</p>
+          <div className="main-proyect-list-item-group-button">
+            <button type="button" className="main-proyect-list-item-button">DEMO</button>
+            <button type="button" className="main-proyect-list-item-button">GITHUB</button>
+          </div>
+        </div>
+        <div className="main-proyect-list-item">
+          <h4 className="main-proyect-list-item-title">Proyect 3</h4>
+          <p className="main-proyect-list-item-subtitle">example text</p>
+          <div className="main-proyect-list-item-group-button">
+            <button type="button" className="main-proyect-list-item-button">DEMO</button>
+            <button type="button" className="main-proyect-list-item-button">GITHUB</button>
+          </div>
+        </div>
+      </div>
+      <h3 id="main-proyect-subtitle">SMALL PROYECTS</h3>
+      <div className="main-proyect-subtitle-list">
+        <div className="main-proyect-subtitle-list-item">
+          <h4 className="main-proyect-subtitle-list-item-title">Example proyect</h4>
+          <p className="main-proyect-subtitle-list-item-subtitle">example text</p>
+          <div className="main-proyect-subtitle-list-item-group-button">
+            <button type="button" className="main-proyect-subtitle-list-item-botton">DEMO</button>
+            <button type="button" className="main-proyect-subtitle-list-item-botton">GITHUB
+            </button>
+          </div>
+        </div>
+        <div className="main-proyect-subtitle-list-item">
+          <h4 className="main-proyect-subtitle-list-item-title">Example proyect</h4>
+          <p className="main-proyect-subtitle-list-item-subtitle">example text</p>
+          <div className="main-proyect-subtitle-list-item-group-button">
+            <button type="button" className="main-proyect-subtitle-list-item-botton">DEMO</button>
+            <button type="button" className="main-proyect-subtitle-list-item-botton">GITHUB
+            </button>
+          </div>
+        </div>
+        <div className="main-proyect-subtitle-list-item">
+          <h4 className="main-proyect-subtitle-list-item-title">Example proyect</h4>
+          <p className="main-proyect-subtitle-list-item-subtitle">example text</p>
+          <div className="main-proyect-subtitle-list-item-group-button">
+            <button type="button" className="main-proyect-subtitle-list-item-botton">DEMO</button>
+            <button type="button" className="main-proyect-subtitle-list-item-botton">GITHUB
+            </button>
+          </div>
+        </div>
+      </div>
+    </section>
+    */ }
+
+export default Proyects;
