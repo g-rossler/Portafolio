22c50905466f6e8ef1497ce5d5bcd12adabee15f
Gerardo Rossler <rossler.gerardo.dev@gmail.com>
2022-05-14 11:39:20 -0300

    Building home style and layout section

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,32 +1,55 @@
 import {
-  Flex, Heading, ButtonGroup, Button, Text,
+  VStack,
+  Heading,
+  Button,
+  Text,
+  ButtonGroup,
+  Stack,
 } from '@chakra-ui/react';
 
 function Home() {
   return (
-    <Flex
-      direction="column"
-      py={30}
-      px={5}
-      backgroundColor="blue"
-      alignItems="center"
-    >
-      <Heading textAlign="center" py={10}>
-        HELLO MY NAME IS GERARDO ROSSLER
-      </Heading>
-      <Text pb={20}>I&apos;m a front end developer.</Text>
-      <ButtonGroup>
-        <Button type="button" className="main-introduccion-button button-54">
-          PROJECTS
-        </Button>
-        <Button type="button" className="main-introduccion-button button-54">
-          GITHUB
-        </Button>
-        <Button type="button" className="main-introduccion-button button-54">
-          LINKEDIN
-        </Button>
-      </ButtonGroup>
-    </Flex>
+    <VStack w="full" bg="black" py={20} px={100} h="100vh">
+      <VStack w="full" ml={20} mt={20} alignItems="flex-start">
+        <Text
+          pl={1.5}
+          color="white"
+          letterSpacing="1px"
+          textAlign="start"
+          fontSize="2xl"
+          w="full"
+        >
+          My name is:
+        </Text>
+        <Heading
+          w="full"
+          textAlign="start"
+          color="#5FE2D1"
+          size="4xl"
+          letterSpacing="3px"
+        >
+          Rossler Gerardo
+        </Heading>
+        <Text
+          letterSpacing="1px"
+          pl={1.5}
+          color="white"
+          fontSize="2em"
+          textAlign="start"
+          w="full"
+          pt={50}
+        >
+          I&apos;m a Front End Developer.
+        </Text>
+      </VStack>
+      <Stack alignItems="center" pt={20}>
+        <ButtonGroup spacing={10} variant="primary">
+          <Button type="button">PROJECTS</Button>
+          <Button type="button">GITHUB</Button>
+          <Button type="button">LINKEDIN</Button>
+        </ButtonGroup>
+      </Stack>
+    </VStack>
   );
 }
 
