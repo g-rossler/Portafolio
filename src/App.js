9534cc73834d8142b1e2b196e2aa17f539621201
Gerardo Rossler <rossler.gerardo.dev@gmail.com>
2022-05-16 21:58:01 -0300

    Add animation

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Proyects from './components/Proyects';
 
 export default function App() {
   return (
-    <Container maxWidth="full" h="100vh" m={0} p={0}>
+    <Container maxWidth="full" m={0} p={0}>
       <Home />
       <About />
       <Proyects />
