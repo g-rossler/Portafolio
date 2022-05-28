632633fb07925f7d54105bc34fdae2c6d5c1ca98
Gerardo Rossler <rossler.gerardo.dev@gmail.com>
2022-05-27 21:37:48 -0300

    Add new Variant to the theme

--- src/themes/index.js
+++ src/themes/index.js
@@ -128,6 +128,38 @@ const theme = extendTheme({
               '1px 1px 0px 0px #FFFFFF, 2px 2px 0px 0px #FFFFFF, 3px 3px 0px 0px #FFFFFF, 4px 4px 0px 0px #FFFFFF, 5px 5px 0px 0px #FFFFFF',
           },
         },
+        terciarySmall: {
+          fontFamily: 'Koulen',
+          fontSize: '18px',
+          letterSpacing: '2px',
+          textDecoration: 'none',
+          color: '#000',
+          cursor: 'pointer',
+          border: '3px solid #5FE2D1',
+          padding: '0.25em 0.5em',
+          boxShadow:
+            '1px 1px 0px 0px #5FE2D1, 2px 2px 0px 0px #5FE2D1, 3px 3px 0px 0px #5FE2D1, 4px 4px 0px 0px #5FE2D1, 5px 5px 0px 0px #5FE2D1',
+          userSelect: 'none',
+          bg: 'white',
+          minW: '90px',
+          _active: {
+            boxShadow: '0px 0px 0px 0px',
+            top: '5px',
+            left: '5px',
+          },
+          _focus: {
+            boxShadow:
+              '1px 1px 0px 0px #5FE2D1, 2px 2px 0px 0px #5FE2D1, 3px 3px 0px 0px #5FE2D1, 4px 4px 0px 0px #5FE2D1, 5px 5px 0px 0px #5FE2D1',
+            ringColor: 'none',
+          },
+          _hover: {
+            bg: 'black',
+            color: 'white',
+            border: '3px solid #FFFFFF',
+            boxShadow:
+              '1px 1px 0px 0px #FFFFFF, 2px 2px 0px 0px #FFFFFF, 3px 3px 0px 0px #FFFFFF, 4px 4px 0px 0px #FFFFFF, 5px 5px 0px 0px #FFFFFF',
+          },
+        },
       },
     },
   },
