682a82a71b11cfb3502e5743ed3bab3b493f8c66
g-rossler <rossler.gerardo.dev@gmail.com>
2024-06-30 19:56:59 -0300

    Update app css properties

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import { Container } from '@chakra-ui/react';
-import Home from './components/Home';
-import About from './components/About';
-import Projects from './components/Projects';
+import { Container } from "@chakra-ui/react";
+import Home from "./components/Home";
+import About from "./components/About";
+import Projects from "./components/Projects";
 
 export default function App() {
   return (
-    <Container maxWidth="full" m={0} p={0} centerContent bg="rgb(32 32 32)">
+    <Container
+      maxW="100%"
+      minW="350px"
+      centerContent
+      m={0}
+      p={0}
+      bg="rgb(32 32 32)"
+    >
       <Home />
       <About />
       <Projects />
